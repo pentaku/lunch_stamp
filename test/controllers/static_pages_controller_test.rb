955e6d38916f58e745896ba79c799f05c587ea01
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-04-05 16:51:07 +0900

    fix: ルーティング変更に伴いコントローラテストを修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,9 +5,4 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get root_url
     assert_response :success
   end
-
-  test "should get home" do
-    get home_url
-    assert_response :success
-  end
 end
