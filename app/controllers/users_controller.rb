f9cea7ef88d8c9f4e3dd8939f99eed515593442f
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-04-19 12:17:53 +0900

    feat: ユーザー登録成功時にフラッシュメッセージを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Lunch Stamp!"
       redirect_to @user
     else
       render 'new', status: :unprocessable_entity
