870c7bec5233e02e10352278efedbcc714281b92
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-04-20 19:37:25 +0900

    feat: ログイン認証処理を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      flash.now[:danger] = "メールアドレスまたはパスワードが正しくありません"
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
