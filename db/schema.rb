2a0e080506e0aaaf585c46560866e3a1de719cb3
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-04-23 09:11:15 +0900

    feat: 永続ログイン機能のためremember_digestカラムをusersテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_04_07_015900) do
+ActiveRecord::Schema[7.0].define(version: 2026_04_23_000957) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
 end
