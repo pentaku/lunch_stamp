be2ce45a251ed7a6e04cdbf90253e2d7c2330ca9
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-01 16:00:35 +0900

    feat: アカウント有効化用のカラムをusersテーブルに追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_04_23_000957) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_01_065203) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,6 +18,9 @@ ActiveRecord::Schema[7.0].define(version: 2026_04_23_000957) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
 end
