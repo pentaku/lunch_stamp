cf7674773f507577587c3ffd9636257ce7c7e245
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-01 16:18:50 +0900

    test: fixtureにuser.activatedとactivated_atを追加し、開発用ユーザーをseedに追加

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_01_065203) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# メインのサンプルユーザーを1人作成する
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             activated: true,
+             activated_at: Time.zone.now)
