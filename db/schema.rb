c1285bf009c292e7d695d99082fd4de1e6ffe979
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-02 19:01:16 +0900

    feat: パスワード再設定リクエスト処理を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2026_05_01_065203) do
+ActiveRecord::Schema[7.0].define(version: 2026_05_02_095103) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2026_05_01_065203) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
