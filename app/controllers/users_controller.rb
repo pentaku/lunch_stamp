d8a1e66900d0eea4fd066194c1a4bbc64f78f601
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-02 09:33:22 +0900

    feat: ユーザー登録時にアカウント有効化メールを送信するよう変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,10 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      reset_session
-      log_in @user
-      flash[:success] = "Welcome to the Lunch Stamp!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "メールを確認してアカウントを有効化してください"
+      redirect_to root_url
     else
       render 'new', status: :unprocessable_entity
     end
