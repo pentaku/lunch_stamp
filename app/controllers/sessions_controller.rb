e95a26d91fe4ef6619705e7ba910212cabe0e04d
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-02 11:13:58 +0900

    feat: アカウント有効化リンクによるユーザー有効化処理を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,15 +4,21 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      reset_session
-      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      log_in user
-      redirect_to forwarding_url || user
+
+    if user&.authenticate(params[:session][:password])
+      if user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        log_in user
+        redirect_to forwarding_url || user
+      else
+        flash[:warning] = "メールを確認してアカウントを有効化してください"
+        redirect_to root_url
+      end
     else
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new', status: :unprocessable_entity
+      flash.now[:danger] = "メールアドレスまたはパスワードが正しくありません"
+      render "new", status: :unprocessable_entity
     end
   end
 
