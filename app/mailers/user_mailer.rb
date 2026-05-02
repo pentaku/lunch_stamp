2caeede618ea109ab78386a409ccfefefb54b833
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-02 19:09:12 +0900

    fix: UserMailerのpassword_resetメソッドを引数対応に修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,9 +5,8 @@ class UserMailer < ApplicationMailer
     mail to: user.email, subject: "【Lunch Stamp】アカウント有効化のご案内"
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset(user)
+    @user = user
+    mail to: user.email, subject: "【Lunch Stamp】パスワード再設定のご案内"
   end
 end
