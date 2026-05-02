ac91fc5395d8ac50b5a4292498a3011c2b5b2b99
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-02 22:37:01 +0900

    test: パスワード再設定メールのテストを追加

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -15,4 +15,19 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match user.activation_token, body
     assert_match CGI.escape(user.email), body
   end
+
+  test "password_reset" do
+    user = users(:michael)
+    user.reset_token = User.new_token
+    mail = UserMailer.password_reset(user)
+
+    body = [mail.text_part&.body&.decoded,
+            mail.html_part&.body&.decoded].compact.join
+
+    assert_equal "【Lunch Stamp】パスワード再設定のご案内", mail.subject
+    assert_equal [user.email], mail.to
+    assert_equal ["ohara1728136@gmail.com"], mail.from
+    assert_match user.reset_token, body
+    assert_match CGI.escape(user.email), body
+  end
 end
