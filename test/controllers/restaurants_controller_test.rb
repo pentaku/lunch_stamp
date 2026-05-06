25683c2fb92757660ec78cbb039264b4384eb221
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-06 13:48:22 +0900

    test: RestaurantsControllerのindexテストを追加

--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,38 +1,8 @@
 require "test_helper"
 
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get restaurants_index_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get restaurants_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get restaurants_create_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get restaurants_show_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get restaurants_edit_url
-    assert_response :success
-  end
-
-  test "should get update" do
-    get restaurants_update_url
-    assert_response :success
-  end
-
-  test "should get destroy" do
-    get restaurants_destroy_url
+  test "indexにアクセスできる" do
+    get restaurants_path
     assert_response :success
   end
 end
