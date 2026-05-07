34956ccccccf5c7686ca0ea52102b6e3e40ed4f1
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-07 12:13:21 +0900

    feat: VisitsController作成と店舗詳細ページに訪問機能を追加

---
+++ app/controllers/visits_controller.rb
@@ -0,0 +1,42 @@
+class VisitsController < ApplicationController
+  before_action :logged_in_user
+
+  def create
+    shop = HotpepperService.find(params[:restaurant_hotpepper_id])
+
+    if shop.nil?
+      return redirect_to restaurants_path,
+                         alert: "店舗情報が取得できませんでした"
+    end
+
+    ActiveRecord::Base.transaction do
+      restaurant = Restaurant.find_or_initialize_by(
+        hotpepper_id: shop["id"]
+      )
+
+      restaurant.assign_attributes(
+        name:      shop["name"],
+        address:   shop["address"],
+        genre:     shop.dig("genre", "name"),
+        area:      shop.dig("small_area", "name"),
+        budget:    shop.dig("budget", "name"),
+        photo_url: shop.dig("photo", "pc", "l"),
+        url:       shop["urls"]["pc"]
+      )
+
+      restaurant.save!
+
+      restaurant.visits.create!(
+        user: current_user,
+        visited_at: Date.current
+      )
+    end
+
+    redirect_to restaurant_path(params[:restaurant_hotpepper_id]),
+                notice: "訪問済みにしました！"
+
+  rescue ActiveRecord::RecordInvalid
+    redirect_to restaurant_path(params[:restaurant_hotpepper_id]),
+                alert: "訪問記録の保存に失敗しました"
+  end
+end
