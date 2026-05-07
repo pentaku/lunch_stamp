8cd66b0ebe25e37e730dc6be1b0ad72f84d8755f
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-07 11:54:37 +0900

    feat: restaurants配下にvisitルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  root   "static_pages#home"
-  get    "/signup",  to: "users#new"
+  root "static_pages#home"
+
+  get    "/signup", to: "users#new"
   get    "/login",  to: "sessions#new"
   post   "/login",  to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
+
   resources :users
   resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
-  resources :restaurants, param: :hotpepper_id
+  resources :password_resets, only: [:new, :create, :edit, :update]
+
+  resources :restaurants, param: :hotpepper_id do
+    resource :visit, only: [:create]
+  end
 end
