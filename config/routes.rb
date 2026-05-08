4b097c72331a7110c447044a03981b46188aa8b1
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-08 23:12:03 +0900

    feat: 利用規約ページとテストを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root "static_pages#home"
+  get "/terms", to: "static_pages#terms"
 
   get    "/signup", to: "users#new"
   get    "/login",  to: "sessions#new"
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,28 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get root" do
+  test "トップページにアクセスできる" do
     get root_url
     assert_response :success
+  end
+
+  test "利用規約ページに200でアクセスできる" do
+    get terms_path
+    assert_response :success
+  end
+
+  test "利用規約ページにタイトルが表示される" do
+    get terms_path
+    assert_select "h1.section-title-text", text: "利用規約"
+  end
+
+  test "利用規約ページに第1条が表示される" do
+    get terms_path
+    assert_match "第1条", response.body
+  end
+
+  test "利用規約ページに第15条が表示される" do
+    get terms_path
+    assert_match "第15条", response.body
   end
 end
