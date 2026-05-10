fd682c08a0339849f694a086b34be0cfa5670e3e
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-10 21:30:21 +0900

    feat: 店舗一覧と店舗詳細にログイン制限を追加

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,6 @@
 class RestaurantsController < ApplicationController
+  before_action :logged_in_user, only: [:index, :show]
+
   def index
     if params[:keyword].present? || params[:genre].present? || params[:budget].present?
       @restaurants = HotpepperService.search(
@@ -14,8 +16,7 @@ class RestaurantsController < ApplicationController
   def show
     @shop = HotpepperService.find(params[:hotpepper_id])
     if @shop.blank?
-      redirect_to restaurants_path,
-            alert: "店舗情報が取得できませんでした"
+      redirect_to restaurants_path, alert: "店舗情報が取得できませんでした"
     end
   end
 end
--- test/controllers/restaurants_controller_test.rb
+++ test/controllers/restaurants_controller_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class RestaurantsControllerTest < ActionDispatch::IntegrationTest
-  test "indexにアクセスできる" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "未ログインの場合、indexはログインページへリダイレクトされる" do
+    get restaurants_path
+    assert_redirected_to login_url
+  end
+
+  test "ログイン済みの場合、indexにアクセスできる" do
+    log_in_as(@user)
     get restaurants_path
     assert_response :success
   end
