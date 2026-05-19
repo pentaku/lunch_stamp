9f1ae4e577d5086881c473fc6a9f3e3f8933d5d0
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-19 23:10:09 +0900

    feat: SessionsControllerにログイン・ログアウトの監査ログを追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,7 @@ class SessionsController < ApplicationController
         reset_session
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         log_in user
+        create_audit_log(action: "login")
         redirect_to forwarding_url || user
       else
         flash[:warning] = "メールを確認してアカウントを有効化してください"
@@ -26,7 +27,10 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out if logged_in?
+    if logged_in?
+      create_audit_log(action: "logout")
+      log_out
+    end
     redirect_to root_url, status: :see_other
   end
 end
