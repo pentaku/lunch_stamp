662b7a7f3ef0c0138a75bfdda5aba701ddcb58f0
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-21 19:19:00 +0900

    feat: 管理画面追加・一時的な管理者昇格シードの設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,21 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 # メインのサンプルユーザーを1人作成する
-User.create!(name: "Example User",
-             email: "ohara1728136@gmail.com",
-             password: "password",
-             password_confirmation: "password",
-             activated: true,
-             activated_at: Time.zone.now)
+  admin_user = User.find_or_create_by!(email: "ohara1728136@gmail.com") do |user|
+    user.name = "Example User"
+    user.password = "password"
+    user.password_confirmation = "password"
+    user.activated = true
+    user.activated_at = Time.zone.now
+  end
+
+  if Rails.env.production?
+    admin_user = User.find_by(email: "ohara1728136@gmail.com")
+
+    if admin_user
+      admin_user.update!(admin: true)
+      puts "====== [SUCCESS] Admin privilege granted to #{admin_user.email} ======"
+    else
+      puts "====== [WARNING] User not found in production database ======"
+    end
+  end
