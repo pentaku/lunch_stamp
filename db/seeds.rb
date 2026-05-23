773e73e3f2bb4ee2b790e717152c301ed8821743
野島一平 <nojimaippei@nojimaippeinoMacBook-Pro.local>
2026-05-23 13:19:30 +0900

    chore: 一時的なseed実行設定を削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,18 +8,3 @@
 # メインのサンプルユーザーを1人作成する
 # 本番環境で既存ユーザーを管理者に昇格させる一時処理
 # テストアカウント作成
-admin_user = User.find_by(email: "admin@example.com")
-
-if admin_user
-  random_password = SecureRandom.urlsafe_base64(32)
-
-  admin_user.update!(
-    password: random_password,
-    password_confirmation: random_password,
-    admin: false
-  )
-
-  puts "====== [SUCCESS] admin@example.com has been disabled ======"
-else
-  puts "====== [INFO] admin@example.com was not found ======"
-end
